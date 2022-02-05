ff9836d791573659dc7f210bd87f2b8a5c611427
MangriMen <mangrimen@gmail.com>
2022-02-05 22:18:28 +0700

    PS lab1 done

---
+++ SP/lab1/client.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define BUFFLEN 255
+#define SERVER "127.0.0.1"
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    char buffer[BUFFLEN];
+    struct sockaddr_in serverAddr, clientAddr;
+    struct hostent *hp, *gethostbyname();
+
+    if (argc < 4)
+    {
+        printf("Ввести имя_хоста, порт и сообщение.\n");
+        exit(1);
+    }
+
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("Не получен socket");
+        exit(1);
+    }
+
+    bzero((char *)&serverAddr, sizeof(serverAddr));
+
+    serverAddr.sin_family = AF_INET;
+    hp = gethostbyname(argv[1]);
+    bcopy(hp->h_addr, &serverAddr.sin_addr, hp->h_length);
+    serverAddr.sin_port = htons(atoi(argv[2]));
+
+    bzero((char *)&clientAddr, sizeof(clientAddr));
+
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_addr.s_addr = INADDR_ANY;
+    clientAddr.sin_port = 0;
+
+    // if (bind(sock, &clientAddr, sizeof(clientAddr))){
+    //     perror("Клиент не получил порт.");
+    //     exit(1);
+    // }
+
+    printf("Клиент: Готов к пересылке.\n");
+    while (true)
+    {
+        if (sendto(sock, argv[3], strlen(argv[3]), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+        {
+            perror("Проблемы с отправкой на сервер.");
+            exit(1);
+        }
+
+        bzero(buffer, sizeof(BUFFLEN));
+        int slen = sizeof(serverAddr);
+
+        if ((recvfrom(sock, (char *)buffer, BUFFLEN, 0,
+                      (struct sockaddr *)&serverAddr, (socklen_t *)&slen)) == -1)
+        {
+            perror("Неверный сокет сервера.");
+            exit(1);
+        }
+
+        printf("Возврат: %s\n", buffer);
+
+        sleep(atoi(argv[3]));
+    }
+
+    printf("Клиент: Пересылка завершена.\n");
+    close(sock);
+}
---
+++ SP/lab1/server.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define BUFFLEN 255
+#define SERVER "127.0.0.1"
+
+void processingMessage(char *in_message, char *out_message, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        out_message[i] = toupper(in_message[i]);
+    }
+}
+
+int main()
+{
+    int socketMain = 0, msgLength = 0;
+    struct sockaddr_in serverAddr, clientAddr;
+    char string_buff[BUFFLEN] = {'\0'};
+    char in_buff[BUFFLEN] = {'\0'};
+    char out_buff[BUFFLEN] = {'\0'};
+
+    if ((socketMain = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("Сервер не в состоянии открыть сокет.");
+        exit(1);
+    }
+
+    memset((char *)&serverAddr, 0, sizeof(serverAddr));
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = 0;
+
+    if (bind(socketMain, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)))
+    {
+        perror("Связывание сервера прошло неудачно.");
+        exit(1);
+    }
+
+    socklen_t length = sizeof(serverAddr);
+
+    if (getsockname(socketMain, (struct sockaddr *)&serverAddr, &length))
+    {
+        perror("Вызов getsockname неудачен.");
+        exit(1);
+    }
+
+    inet_ntop(AF_INET, &serverAddr.sin_addr, string_buff, BUFFLEN);
+    printf("Сервер:\n");
+    printf("\tIP адрес: %s\n", string_buff);
+    printf("\tПорт: %d\n\n", ntohs(serverAddr.sin_port));
+
+    listen(socketMain, 2);
+
+    while (1)
+    {
+        length = sizeof(clientAddr);
+        memset(in_buff, 0, BUFFLEN);
+        memset(out_buff, 0, BUFFLEN);
+        memset(string_buff, 0, BUFFLEN);
+
+        if ((msgLength = recvfrom(socketMain, in_buff, BUFFLEN, 0, (struct sockaddr *)&clientAddr, &length)) < 0)
+        {
+            perror("Неверный сокет клиента.");
+            exit(1);
+        }
+
+        processingMessage(in_buff, out_buff, msgLength);
+
+        if (sendto(socketMain, out_buff, msgLength, 0, (struct sockaddr *)&clientAddr, length) < 0)
+        {
+            perror("Проблемы с отправкой на клиент.");
+            exit(1);
+        }
+
+        inet_ntop(AF_INET, &clientAddr.sin_addr, string_buff, BUFFLEN);
+        printf("Сервер:\n");
+        printf("\tIP адрес клиента: %s\n", string_buff);
+        printf("\tPort клиента: %d\n", ntohs(clientAddr.sin_port));
+        printf("\tДлина сообщения - %d\n", msgLength);
+        printf("\tСообщение: %s\n\n", in_buff);
+    }
+}
