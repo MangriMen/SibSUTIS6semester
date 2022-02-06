f7c45663e97dc0408519775496a2b6e70061d983
MangriMen <mangrimen@gmail.com>
2022-02-06 18:13:31 +0700

    PGP lab1 done

--- PGP/lab1/lab1.cu
+++ PGP/lab1/lab1.cu
@@ -1,32 +1,100 @@
-#include <cuda.h>
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <fstream>
 
-__global__ void gTest(float *a)
+using namespace std;
+
+__global__ void vector_add(float a[], float b[])
 {
-  a[threadIdx.x + blockDim.x * blockIdx.x] =
-      (float)(threadIdx.x + blockDim.x * blockIdx.x);
+  a[threadIdx.x + blockDim.x * blockIdx.x] += b[threadIdx.x + blockDim.x * blockIdx.x];
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-  float *da, *ha;
-  int num_of_blocks = 10, threads_per_block = 32;
-  int N = num_of_blocks * threads_per_block;
+  ofstream file_out("out.csv");
+  cout.rdbuf(file_out.rdbuf());
 
-  ha = (float *)calloc(N, sizeof(float));
-  cudaMalloc((void **)&da, N * sizeof(float));
+  size_t start_threads = 2;
+  size_t max_threads = 1 << 10;
 
-  gTest<<<dim3(num_of_blocks),
-          dim3(threads_per_block)>>>(da);
+  if (argc > 2)
+  {
+    start_threads = stoll(argv[1]);
+    max_threads = stoll(argv[1]);
+  }
 
-  cudaMemcpy(ha, da, N * sizeof(float),
-             cudaMemcpyDeviceToHost);
+  size_t N = pow(2, 20);
+  for (size_t threads = start_threads; threads <= max_threads; threads = threads << 1)
+  {
+    size_t blocks = N / threads;
 
-  for (int i = 0; i < N; i++)
-    printf("%g\n", ha[i]);
+    float *arr1 = new float[N];
+    float *arr2 = new float[N];
+    float *result = new float[N];
 
-  free(ha);
-  cudaFree(da);
+    for (size_t i = 0; i < N; i++)
+    {
+      arr1[i] = static_cast<float>(i);
+      arr2[i] = static_cast<float>(i);
+    }
+
+    float *cu_arr1 = nullptr;
+    float *cu_arr2 = nullptr;
+
+    if (cudaMalloc((void **)&cu_arr1, N * sizeof(float)) != cudaSuccess)
+    {
+      cerr << "Error when memory allocation for cuda";
+      return -1;
+    }
+
+    if (cudaMalloc((void **)&cu_arr2, N * sizeof(float)) != cudaSuccess)
+    {
+      cerr << "Error when memory allocation for cuda";
+      return -1;
+    }
+
+    if (cudaMemcpy(cu_arr1, arr1, N * sizeof(float), cudaMemcpyHostToDevice) != cudaSuccess)
+    {
+      cerr << "Error when copy memory from host to device";
+      return -1;
+    }
+
+    if (cudaMemcpy(cu_arr2, arr2, N * sizeof(float), cudaMemcpyHostToDevice) != cudaSuccess)
+    {
+      cerr << "Error when copy memory from host to device";
+      return -1;
+    }
+
+    auto timer_start = chrono::high_resolution_clock::now();
+    vector_add<<<dim3(blocks), dim3(threads)>>>(cu_arr1, cu_arr2);
+    auto timer_stop = chrono::high_resolution_clock::now();
+
+    auto duration = chrono::duration_cast<chrono::nanoseconds>(timer_stop - timer_start);
+    cout << threads << ";" << duration.count() << endl;
+
+    if (cudaMemcpy(result, cu_arr1, N * sizeof(float), cudaMemcpyDeviceToHost))
+    {
+      cerr << "Error when copy memory from device to host";
+      return -1;
+    }
+
+    for (size_t i = 0; i < N; i++)
+    {
+      if (result[i] != arr1[i] + arr2[i])
+      {
+        cout << "result[i] != arr1[i] + arr2[i]" << endl;
+        return -1;
+      }
+    }
+
+    cudaFree(&cu_arr1);
+    cudaFree(&cu_arr2);
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] result;
+  }
 
   return 0;
 }
---
+++ PGP/lab1/make.bat
@@ -0,0 +1,2 @@
+call "C:\Programs\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+nvcc lab1.cu -o lab1.exe
