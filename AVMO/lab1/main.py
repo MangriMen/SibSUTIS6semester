10767a74660c57bad939a5e6ed7cf20c3e72dfd4
MangriMen <mangrimen@gmail.com>
2022-02-06 16:39:10 +0700

    AVMO lab1 second solution, improve fraction class

--- AVMO/lab1/main.py
+++ AVMO/lab1/main.py
@@ -1,83 +1,6 @@
 import math
 import sys
-
-
-class Fractions:
-    def __init__(self, numerator=0, denominator=1):
-        self.numerator = numerator
-        self.denominator = denominator
-
-    def normalize(self):
-        if self.numerator == 0:
-            self.denominator = 1
-            return
-
-        gcd = math.gcd(int(self.numerator), int(self.denominator))
-        self.numerator /= gcd
-        self.denominator /= gcd
-
-        if self.denominator < 0:
-            self.denominator *= -1
-            self.numerator *= -1
-
-    def sign(self):
-        if self.numerator < 0:
-            return "-"
-
-        return "+"
-
-    def __add__(self, other):
-        temp = Fractions()
-        temp.numerator = self.numerator * other.denominator \
-            + self.denominator * other.numerator
-        temp.denominator = self.denominator * other.denominator
-        temp.normalize()
-
-        return temp
-
-    def __sub__(self, other):
-        temp = Fractions()
-        temp.numerator = self.numerator * other.denominator \
-            - self.denominator * other.numerator
-        temp.denominator = self.denominator * other.denominator
-        temp.normalize()
-
-        return temp
-
-    def __mul__(self, other):
-        temp = Fractions()
-        temp.numerator = self.numerator * other.numerator
-        temp.denominator = self.denominator * other.denominator
-        temp.normalize()
-
-        return temp
-
-    def __truediv__(self, other):
-        temp = Fractions()
-        temp.numerator = self.numerator * other.denominator
-        temp.denominator = self.denominator * other.numerator
-        temp.normalize()
-
-        return temp
-
-    def __eq__(self, other):
-        if isinstance(other, Fractions):
-            if self.numerator == other.numerator \
-                    and self.denominator == other.denominator:
-                return True
-        elif isinstance(other, int):
-            if self.numerator == other and self.denominator == 1:
-                return True
-
-        return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __str__(self):
-        if self.denominator == 1:
-            return f"{int(self.numerator)}"
-        return f"{int(self.numerator)}/{int(self.denominator)}"
+from my_fraction import Fraction
 
 
 class Solver:
@@ -95,7 +18,7 @@ class Solver:
 
         self.matrix = []
         for line in lines:
-            self.matrix.append([Fractions(x, 1) for x in map(
+            self.matrix.append([Fraction(x, 1) for x in map(
                 lambda item: int(item), line.split(" "))])
 
     def solve(self):
@@ -118,7 +41,7 @@ class Solver:
                     continue
                 self.matrix[row][col] /= self.matrix[row][row]
 
-            self.matrix[row][row] = Fractions(1, 1)
+            self.matrix[row][row] = Fraction(1, 1)
 
             for i in range(len(self.matrix)):
                 if i == row:
