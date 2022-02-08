7230dadd3a9aaed9a3d3e2afe5d0b00461bbb946
NPanin <subject6gfrt@gmail.com>
2022-02-08 19:47:49 +0700

    PGP | Added lab 1 & 2

--- PGP/lab2/make.bat
+++ PGP/lab2/make.bat
@@ -1,2 +1,2 @@
-call "C:\Programs\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 nvcc lab2.cu -o lab2.exe
