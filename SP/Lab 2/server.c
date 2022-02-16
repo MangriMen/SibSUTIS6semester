6fd1d015370e79212fab6f0b2635969e0438d7a3
NPanin <subject6gfrt@gmail.com>
2022-02-16 23:26:56 +0700

    SP | lab 2 & lab 3 modified & added

--- SP/Lab 2/server.c
+++ SP/Lab 2/server.c
@@ -11,15 +11,16 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <stdbool.h>
+#include <signal.h>
 
-#define BUFLEN 81
+#define BUFLEN 255
 #define SOCKET_COUNT 256
 
-void BuffWork(int sockClient)
+void ProcessMessage(int sockClient)
 {
     char buf[BUFLEN];
-    int msgLength;
-    bzero(buf, BUFLEN);
+    int msgLength = 0;
+    memset((char *)&buf, 0, sizeof(buf));
 
     if ((msgLength = recv(sockClient, buf, BUFLEN, 0)) < 0)
     {
@@ -27,14 +28,23 @@ void BuffWork(int sockClient)
         exit(1);
     }
 
-    printf("SERVER: Socket дляклиента-%d\n", sockClient);
-    printf("SERVER: Длинасообщения-%d\n", msgLength);
-    printf("SERVER: Сообщение: %s\n\n", buf);
+    if (msgLength == 0)
+    {
+        printf("Сервер: клиент с номером %d разорвал соединение с сервером\n\n", sockClient);
+        close(sockClient);
+        exit(0);
+    }
+
+    printf("Сервер: Socket для клиента: %d\n", sockClient);
+    printf("\tДлина сообщения: %d\n", msgLength);
+    printf("\tСообщение: %s\n\n", buf);
 }
 
 int main()
 {
-    int sockMain, length;
+    signal(SIGCHLD, SIG_IGN);
+
+    int sockMain;
     struct sockaddr_in servAddr;
 
     if ((sockMain = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -43,64 +53,47 @@ int main()
         exit(1);
     }
 
-    bzero((char *)&servAddr, sizeof(servAddr));
-
+    memset((char *)&servAddr, 0, sizeof(servAddr));
     servAddr.sin_family = AF_INET;
     servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servAddr.sin_port = 0;
 
-    if (bind(sockMain, (struct sockaddr *)&servAddr, sizeof(servAddr)))
+    socklen_t length = sizeof(servAddr);
+    if (bind(sockMain, (struct sockaddr *)&servAddr, length))
     {
         perror("Связывание сервера неудачно.");
         exit(1);
     }
 
-    length = sizeof(servAddr);
-
-    if (getsockname(sockMain, (struct sockaddr *)&servAddr, (socklen_t *restrict)&length))
+    if (getsockname(sockMain, (struct sockaddr *)&servAddr, &length))
     {
         perror("Вызов getsockname неудачен.");
         exit(1);
     }
 
-    printf("СЕРВЕР: номер порта-% d\n", ntohs(servAddr.sin_port));
+    char string_buff[BUFLEN] = {'\0'};
+    inet_ntop(AF_INET, &servAddr.sin_addr, string_buff, BUFLEN);
+    printf("Сервер:\n");
+    printf("\tIP:%s:%d\n\n", string_buff, ntohs(servAddr.sin_port));
+
     listen(sockMain, 5);
 
-    int *sockClients = (int *)malloc(sizeof(int) * SOCKET_COUNT);
-    int *sockClientsPIDs = (int *)malloc(sizeof(int) * SOCKET_COUNT);
-    int sockClientsCounter = 0;
     while (true)
     {
-        if (sockClientsCounter > 255)
-        {
-            perror("Кончились socketы.");
-            exit(1);
-        }
-
-        if ((sockClients[sockClientsCounter] = accept(sockMain, 0, 0)) < 0)
+        int socketClient = 0;
+        if ((socketClient = accept(sockMain, 0, 0)) < 0)
         {
             perror("Неверный socket для клиента.");
             exit(1);
         }
 
-        int forkedPID = fork();
-        if (forkedPID > 0)
+        if (fork() == 0)
         {
-            sockClientsPIDs[sockClientsCounter] = forkedPID;
+            while (true)
+            {
+                ProcessMessage(socketClient);
+            }
         }
-        else if (forkedPID == 0)
-        {
-            BuffWork(sockClients[sockClientsCounter]);
-        }
-
-        // close(sockClient);
-        sockClientsCounter++;
-    }
-
-    for (int i = 0; i < sockClientsCounter; ++i)
-    {
-        close(sockClients[i]);
-        kill(sockClientsPIDs[i]);
     }
 
     return 0;
