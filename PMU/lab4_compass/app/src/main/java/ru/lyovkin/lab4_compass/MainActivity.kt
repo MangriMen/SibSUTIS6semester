fb9b2a13fa92b1930e3d5bf7140ecd8bda40bdf5
MangriMen <mangrimen@gmail.com>
2022-02-19 14:49:25 +0700

    PMU lab4 removed unnecessary import

--- PMU/lab4_compass/app/src/main/java/ru/lyovkin/lab4_compass/MainActivity.kt
+++ PMU/lab4_compass/app/src/main/java/ru/lyovkin/lab4_compass/MainActivity.kt
@@ -11,7 +11,6 @@ import android.view.animation.RotateAnimation
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.math.abs
 import kotlin.math.round
 
 
