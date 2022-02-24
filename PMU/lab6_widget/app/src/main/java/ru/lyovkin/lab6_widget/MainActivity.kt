b368e4c067b5393653a36616254f28bb432de850
MangriMen <mangrimen@gmail.com>
2022-02-25 00:37:14 +0700

    PMU lab6_widget done

--- PMU/lab6_widget/app/src/main/java/ru/lyovkin/lab6_widget/MainActivity.kt
+++ PMU/lab6_widget/app/src/main/java/ru/lyovkin/lab6_widget/MainActivity.kt
@@ -18,18 +18,10 @@ import java.util.concurrent.LinkedBlockingQueue
 import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
-    private val cbrURL: String = "https://www.cbr.ru/scripts/xml_metall.asp"
-    private val urlCharset = StandardCharsets.UTF_8.name()
-
-    private val metals = mapOf (
-        "1" to "Золото",
-        "2" to "Серебро",
-        "3" to "Платина",
-        "4" to "Палладий"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
         onUpdate(null)
@@ -43,64 +35,78 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getData(): MutableMap<String, String> {
-        var data = mutableMapOf<String, String>()
-        val records = readXml(fetchData()).getElementsByTagName("Record")
-        data["Дата"] = records.item(records.length - 1).attributes.getNamedItem("Date").textContent
-        for (record in records.length-4 until records.length) {
-            val rec = records.item(record)
-            data["${metals[rec.attributes.getNamedItem("Code").textContent]}"] = "${rec.childNodes.item(0).textContent}/${rec.childNodes.item(1).textContent}"
-        }
-        return data
-    }
+    companion object {
+        private const val cbrURL: String = "https://www.cbr.ru/scripts/xml_metall.asp"
+        private val urlCharset = StandardCharsets.UTF_8.name()
 
-    private fun getDateRequest(): String {
-        val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale("ru"))
-        val calendar = GregorianCalendar()
-
-        val dateEnd = dateFormatter.format(calendar.time)
-
-        calendar.add(Calendar.DAY_OF_MONTH, -10)
-        val dateStart = dateFormatter.format(calendar.time)
-
-        return java.lang.String.format(
-            "date_req1=%s&date_req2=%s",
-            URLEncoder.encode(dateStart, urlCharset),
-            URLEncoder.encode(dateEnd, urlCharset)
+        private val metals = mapOf (
+            "1" to "Золото",
+            "2" to "Серебро",
+            "3" to "Платина",
+            "4" to "Палладий"
         )
-    }
 
-    private fun fetchData(): String {
-        val msgQueue = LinkedBlockingQueue<String>()
-
-        Thread {
-            try {
-                val connection: HttpURLConnection = URL("$cbrURL?${getDateRequest()}").openConnection() as HttpURLConnection
-                connection.setRequestProperty("Accept-Charset", urlCharset)
-                val response: InputStream = connection.inputStream
-
-                if (connection.responseCode in 200..399) {
-                    Scanner(response).use { scanner ->
-                        msgQueue.add(scanner.useDelimiter("\\A").next())
-                    }
-                }
-                else {
-                    msgQueue.add("")
-                }
-            } catch (e: Exception) {
-                msgQueue.add("")
-                e.printStackTrace()
+        fun getData(): MutableMap<String, String> {
+            val data = mutableMapOf<String, String>()
+            val records = readXml(fetchData()).getElementsByTagName("Record")
+            data["Дата"] =
+                records.item(records.length - 1).attributes.getNamedItem("Date").textContent
+            for (record in records.length - 4 until records.length) {
+                val rec = records.item(record)
+                data["${metals[rec.attributes.getNamedItem("Code").textContent]}"] =
+                    "${rec.childNodes.item(0).textContent}/${rec.childNodes.item(1).textContent}"
             }
-        }.start()
+            return data
+        }
 
-        return msgQueue.take()
-    }
+        private fun getDateRequest(): String {
+            val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale("ru"))
+            val calendar = GregorianCalendar()
 
-    private fun readXml(xmlString: String): Document {
-        val dbFactory = DocumentBuilderFactory.newInstance()
-        val dBuilder = dbFactory.newDocumentBuilder()
-        val xmlInput = InputSource(StringReader(xmlString))
+            val dateEnd = dateFormatter.format(calendar.time)
 
-        return dBuilder.parse(xmlInput)
+            calendar.add(Calendar.DAY_OF_MONTH, -10)
+            val dateStart = dateFormatter.format(calendar.time)
+
+            return java.lang.String.format(
+                "date_req1=%s&date_req2=%s",
+                URLEncoder.encode(dateStart, urlCharset),
+                URLEncoder.encode(dateEnd, urlCharset)
+            )
+        }
+
+        private fun fetchData(): String {
+            val msgQueue = LinkedBlockingQueue<String>()
+
+            Thread {
+                try {
+                    val connection: HttpURLConnection = URL("$cbrURL?${getDateRequest()}").openConnection() as HttpURLConnection
+                    connection.setRequestProperty("Accept-Charset", urlCharset)
+                    val response: InputStream = connection.inputStream
+
+                    if (connection.responseCode in 200..399) {
+                        Scanner(response).use { scanner ->
+                            msgQueue.add(scanner.useDelimiter("\\A").next())
+                        }
+                    }
+                    else {
+                        msgQueue.add("")
+                    }
+                } catch (e: Exception) {
+                    msgQueue.add("")
+                    e.printStackTrace()
+                }
+            }.start()
+
+            return msgQueue.take()
+        }
+
+        private fun readXml(xmlString: String): Document {
+            val dbFactory = DocumentBuilderFactory.newInstance()
+            val dBuilder = dbFactory.newDocumentBuilder()
+            val xmlInput = InputSource(StringReader(xmlString))
+
+            return dBuilder.parse(xmlInput)
+        }
     }
 }
--- PMU/lab6_widget/app/src/main/java/ru/lyovkin/lab6_widget/MainWidget.kt
+++ PMU/lab6_widget/app/src/main/java/ru/lyovkin/lab6_widget/MainWidget.kt
@@ -1,8 +1,10 @@
 package ru.lyovkin.lab6_widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 
 /**
@@ -16,7 +18,14 @@ class MainWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+            val views = RemoteViews(context.packageName, R.layout.main_widget)
+            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
+
             updateAppWidget(context, appWidgetManager, appWidgetId)
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
@@ -34,7 +43,10 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
+//    val widgetText = context.getString(R.string.appwidget_text)
+    val data = MainActivity.getData()
+    val widgetText = "Дата: ${data["Дата"]}\nЗолото: ${data["Золото"]}\n"
+
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.main_widget)
     views.setTextViewText(R.id.appwidget_text, widgetText)
