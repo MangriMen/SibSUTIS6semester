ede8892936fde4db3505ee3e79d703a10626fda9
MangriMen <mangrimen@gmail.com>
2022-02-27 01:40:39 +0700

    PMU lab6_widget moved intent to updateAppWidget, changed widget preview image

--- PMU/lab6_widget/app/src/main/java/ru/lyovkin/lab6_widget/MainWidget.kt
+++ PMU/lab6_widget/app/src/main/java/ru/lyovkin/lab6_widget/MainWidget.kt
@@ -18,14 +18,7 @@ class MainWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            val intent = Intent(context, MainActivity::class.java)
-            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
-
-            val views = RemoteViews(context.packageName, R.layout.main_widget)
-            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
-
             updateAppWidget(context, appWidgetManager, appWidgetId)
-            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
@@ -50,6 +43,10 @@ internal fun updateAppWidget(
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.main_widget)
     views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    val intent = Intent(context, MainActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
