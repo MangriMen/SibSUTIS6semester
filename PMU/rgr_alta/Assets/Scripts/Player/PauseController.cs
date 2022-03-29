d7abcf71a12f4cc5200503b74d04f59a03aedd5b
MangriMen <mangrimen@gmail.com>
2022-03-29 18:03:20 +0700

    PMU rgr_alta better background, menu, player

--- PMU/rgr_alta/Assets/Scripts/Player/PauseController.cs
+++ PMU/rgr_alta/Assets/Scripts/Player/PauseController.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject pausePrefab;
+
+    private GameObject pause;
+
+    public void onPause()
+    {
+        pause = Instantiate(pausePrefab, transform);
+        pause.transform.Find("You Died").gameObject.SetActive(false);
+        
+        Transform restart = pause.transform.Find("Restart");
+        restart.GetComponent<Button>().onClick.RemoveAllListeners();
+        restart.GetComponent<Button>().onClick.AddListener(onResume);
+        restart.transform.Find("Text").GetComponent<Text>().text = "Продолжить";
+        
+        Time.timeScale = 0f;
+    }
+
+    public void onResume()
+    {
+        Time.timeScale = 1f;
+        Destroy(pause);
+    }
+
     public void onRestart()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void onMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
 }
--- PMU/rgr_alta/Assets/Scripts/Player/PlayerController.cs
+++ PMU/rgr_alta/Assets/Scripts/Player/PlayerController.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private Vector2 gravity = new Vector2(-9.81f, 0f);
+    private Vector2 gravity = new Vector2(9.81f * 250, 0f);
 
     [SerializeField]
     private GameObject screenOfDeadPrefab;
     [SerializeField]
     private Canvas cnv;
+    [SerializeField]
+    private Text scoreText;
+
+    private float timer = 0;
+    private ulong score = 0;
 
     private GameObject screenOfDead;
 
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
     private bool isGrounded = false;
 
@@ -24,16 +30,31 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        score = 0;
+        scoreText.text = "0";
     }
 
     void Update()
     {
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (isGrounded && (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetButtonDown("Jump")))
         {
             gravity = -gravity;
+            sr.flipX = !sr.flipX;
+            isGrounded = false;
         }
 
-        rb.AddForce(gravity);
+        rb.AddForce(gravity * Time.deltaTime);
+
+        timer += Time.deltaTime;
+
+        if (timer > 0.1f)
+        {
+            score += 1;
+            scoreText.text = score.ToString();
+            timer = 0;
+        }
+
     }
 
     private void FixedUpdate()
@@ -53,6 +74,7 @@ public class PlayerController : MonoBehaviour
                 if (screenOfDead == null)
                 {
                     screenOfDead = Instantiate(screenOfDeadPrefab, cnv.transform);
+                    screenOfDead.transform.Find("Restart").GetComponent<Button>().onClick.AddListener(screenOfDead.GetComponent<PauseController>().onRestart);
                 }
             }
         }
@@ -61,8 +83,8 @@ public class PlayerController : MonoBehaviour
     private List<Collider2D> getLeftRightColliders()
     {
         List<Collider2D> all = new List<Collider2D>();
-        all.AddRange(Physics2D.OverlapCircleAll(left.transform.position, 0.05f));
-        all.AddRange(Physics2D.OverlapCircleAll(right.transform.position, 0.05f));
+        all.AddRange(Physics2D.OverlapCircleAll(left.transform.position, 0.01f));
+        all.AddRange(Physics2D.OverlapCircleAll(right.transform.position, 0.01f));
         return all;
     }
 }
